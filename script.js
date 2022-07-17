8709c2cfd3f2c23158b065b4bd22414bc46a857b
Diego <diferox@gmail.com>
2022-07-16 23:32:05 -0500

    add a winner funtion

--- script.js
+++ script.js
@@ -1,42 +1,122 @@
-let board = document.querySelector('.gameboard');
-let divs = board.querySelectorAll('.divs');
-let sw = true;
-
-
-function displayController()
+(function() 
 {
 
-  switch (sw)
-  {
-    case true:
-      sw = false;
-      playerOne();
-      
-      break;
-    
-    case false:
-      sw = true;
-      playerTwo();
+  let board = document.querySelector('.gameboard');
+  let divs = board.querySelectorAll('button');
+  let boardArr = [];
+  let sw = true;
 
-      break;
+
+  divs.forEach(divs => divs.addEventListener ('click', e => 
+  {
+    e.target.textContent = sw?'X':'O';
+    e.target.disabled = true;
+    sw = !sw;
+
+    addBoard();
+
+    options();
+
+    checkDraw();
+
+  }));
+
+
+  function addBoard()
+  {
+    boardArr[0] = document.getElementById('btn0').textContent;
+    boardArr[1] = document.getElementById('btn1').textContent;
+    boardArr[2] = document.getElementById('btn2').textContent;
+
+    boardArr[3] = document.getElementById('btn3').textContent;
+    boardArr[4] = document.getElementById('btn4').textContent;
+    boardArr[5] = document.getElementById('btn5').textContent;
+
+    boardArr[6] = document.getElementById('btn6').textContent;
+    boardArr[7] = document.getElementById('btn7').textContent;
+    boardArr[8] = document.getElementById('btn8').textContent;
   }
 
-}
 
-function playerOne()
-{
-  divs.forEach(divs => divs.addEventListener ('click', () => 
+  function options()
   {
-    divs.textContent = 'X';
-    divs.disabled = true;
-  }));
-}
+    displayWin(0, 1, 2);
+    displayWin(3, 4, 5);
+    displayWin(6, 7, 8);
 
-function playerTwo()
-{
-  divs.forEach(divs => divs.addEventListener ('click', () => 
+    displayWin(0, 3, 6);
+    displayWin(1, 4, 7);
+    displayWin(2, 5, 8);
+
+    displayWin(0, 4, 8);
+    displayWin(2, 4, 6);
+  }
+
+
+  function displayWin(i, j, k)
   {
-    divs.textContent = 'O';
-    divs.disabled = true;
+    if((boardArr[i] || boardArr[j] || boardArr[k]) != "")
+    {
+      if(boardArr[i] == boardArr[j] && boardArr[j] == boardArr[k])
+      {
+        let showWin = document.getElementById('showWin');
+        let txt = document.getElementById('txt');
+
+        showWin.style.display = 'flex';
+        txt.textContent = "The Winner is ( " + boardArr[i] + " )";
+
+        showWinner(i, j, k);
+      }
+    }
+  }
+
+  
+  function showWinner(i, j, k)
+  {
+    document.getElementById('btn' + i).style.background = 'greenyellow';
+    document.getElementById('btn' + j).style.background = 'greenyellow';
+    document.getElementById('btn' + k).style.background = 'greenyellow';
+  }
+
+
+  let reset = document.getElementById('reset');
+
+  divs.forEach(divs => reset.addEventListener ('click', () => 
+  {
+    divs.textContent = '';
+    divs.style.background = 'white';
+    divs.disabled = false;
+
+    let showWin = document.getElementById('showWin');
+    showWin.style.display = 'none';
+
   }));
-}
+
+
+  function checkDraw()
+  {
+    let draw = false;
+    for(let i = 0; i <= boardArr.length; i++)
+    {
+      if(boardArr[i] != "")
+      {
+        draw = true;
+      }
+      else
+      {
+        return;
+      }
+    }
+
+    if(draw == true)
+    {
+      let showWin = document.getElementById('showWin');
+      let txt = document.getElementById('txt');
+
+      showWin.style.display = 'flex';
+      txt.textContent = "D R A W";
+    }
+  }
+
+
+})();
